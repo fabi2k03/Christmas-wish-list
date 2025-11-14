0d4d75940306cd69e65a79b256345851ee1948ac
fabi_2k03 <fabian.kopf03@gmail.com>
2025-11-14 09:01:26 +0100

    Utlis header defined

---
+++ utils.h
@@ -0,0 +1,20 @@
+//
+// Created by Fabian Kopf on 14.11.25.
+//
+
+#ifndef CHISTMAS_WISHLIST_UTILS_H
+#define CHISTMAS_WISHLIST_UTILS_H
+
+#include <string>
+
+namespace Utlis {
+    void clearInput();
+    void clearScreen();
+    void pause();
+    int getIntInput(const std::string &prompt, int min = INT_MIN, int max = INT_MAX);
+    double getDoubleInput(const std::string &prompt, double min = 0.0);
+    std::string getStringInput(const std::string &prompt);
+    bool confirm(const std::string &message);
+}
+
+#endif //CHISTMAS_WISHLIST_UTILS_H
